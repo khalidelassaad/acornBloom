0d88739c5a3d89499deb94a535fea33016fcffe6
Khalid Elassaad <khalidelassaad@gmail.com>
2022-08-21 15:57:57 -0700

    code: working on rendering horizon

--- main.py
+++ main.py
@@ -3,10 +3,18 @@ import curses
 from animator import startAnimation
 from renderer import renderInitialAcorn, renderWorld
 from utility import startKeyboardListeners
+from time import sleep
+
 
 def runSimulation(cursesScreen):
     renderWorld(cursesScreen)
     renderInitialAcorn(cursesScreen)
+
+    while True:
+        # cursesScreen.clear()
+        cursesScreen.refresh()
+        sleep(1)
+
     startKeyboardListeners()
 
     startAnimation()
--- renderer.py
+++ renderer.py
@@ -1,5 +1,15 @@
 def renderWorld(cursesScreen):
+
+    awoArbitraryWorldObject = [["o" * 80] * 24]
+
+    cursesScreen.addstr(3, 12, 
+    """Hello
+        World
+    """)
     pass
 
 def renderInitialAcorn(cursesScreen):
+    pass
+
+if __name__ == "__name__":
     pass
