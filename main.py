8588dcab04359cf8583ccee6be49d1b829a2fac4
Khalid Elassaad <khalidelassaad@gmail.com>
2022-08-21 16:42:14 -0700

    refactor: de-hardcode screen dimensions

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import curses
 from animator import startAnimation
 from renderer import renderInitialAcorn, renderWorld
 from utility import startKeyboardListeners
-from time import sleep
 
 
 def runSimulation(cursesScreen):
+    height, width = cursesScreen.getmaxyx()
     curses.curs_set(False)
 
-    renderWorld(cursesScreen)
+    worldArray = [["o" for _ in range(width)] for _ in range(height)]
+    renderWorld(cursesScreen, worldArray)
     renderInitialAcorn(cursesScreen)
 
     cursesScreen.refresh() # may not be necessary
--- renderer.py
+++ renderer.py
@@ -1,30 +1,20 @@
 import curses
 
-def renderWorld(cursesScreen):
+def renderWorld(cursesScreen, worldArray):
+    height, width = cursesScreen.getmaxyx()
     cursesScreen.refresh()
-    pad = curses.newpad(24, 81)
 
-    awoArbitraryWorldObject = [["o" for _ in range(80)] for _ in range(24)]
+    pad = curses.newpad(height, width + 1)
 
-    for y in range(24):
-        for x in range(80): # 
-            pad.addch(y,x, awoArbitraryWorldObject[y][x])
+    for y in range(height):
+        for x in range(width):
+            pad.addch(y,x, worldArray[y][x])
 
-    pad.refresh(0, 0, 0, 0, 23, 79)
+    pad.refresh(0, 0, 0, 0, height - 1, width - 1)
 
 def renderInitialAcorn(cursesScreen):
     pass
 
-def awoObjectToString(awoArbitraryWorldObject):
-    awoArbitraryWorldObject = [["o" for _ in range(80)] for _ in range(24)]
-    awoString = ""
-    for line in awoArbitraryWorldObject:
-        for char in line:
-            awoString += char
-        awoString += "\n"
-    return awoString
 
 if __name__ == "__main__":
-    awoString = awoObjectToString([["o" for _ in range(80)] for _ in range(24)])
-
-    print("\n" + awoString)
+    pass
