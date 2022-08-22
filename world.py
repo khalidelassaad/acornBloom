8fd70ed75c7d93394dd954a2f10ef276304aadd1
Khalid Elassaad <khalidelassaad@gmail.com>
2022-08-21 17:24:34 -0700

    refactor: remove transformWorld

--- world.py
+++ world.py
@@ -3,6 +3,3 @@ def initializeWorld(worldArray):
     width = len(worldArray[0])
     horizonLevel = (height * 3) // 5
     worldArray[horizonLevel] = ["-" for _ in range(width)]
-
-def transformWorld(worldArray, i):
-    worldArray[i][i] = "X"
