4f988110f7d8509a27b3db3fea65e3aadc535ead
Khalid Elassaad <khalidelassaad@gmail.com>
2022-08-26 14:16:22 -0700

    code: trunk handles branching

--- tree/branch.py
+++ tree/branch.py
@@ -1,2 +1,14 @@
 class Branch:
-    pass
+    def __init__(self, branchOriginDataDict):
+        self.branchDict = {
+            (0, 0): branchOriginDataDict,
+            (0, 1): {"symbol": "K"}
+        }
+
+        # TODO: add a branch!
+
+    def handleAging(self):
+        pass
+
+    def getBranchDict(self):
+        return self.branchDict
--- tree/trunk.py
+++ tree/trunk.py
@@ -1,5 +1,7 @@
 import random
 
+from tree.branch import Branch
+
 
 class Trunk:
 
@@ -9,6 +11,7 @@ class Trunk:
         self.trunkDict = {
             (0, 0): originDataDict
         }
+        self.branchObjectsDict = dict()
         self.thickeningThreshold = random.choice(range(3, 6))
         self.branchingThreshold = random.choice(range(2, 4))
         self.lengthenProbability = 1
@@ -26,15 +29,37 @@ class Trunk:
             self.lengthenProbability += -0.1
 
     def getTrunkDict(self):
-        return self.trunkDict
+        trunkDict = self.trunkDict.copy()
+        for branchCoords, branchObject in self.branchObjectsDict.items():
+            for branchSegmentCoords, branchSegmentDataDict in branchObject.getBranchDict().items():
+                trunkDictCoords = tuple(sum(x) for x in zip(
+                    branchCoords, branchSegmentCoords))
+                trunkDict[trunkDictCoords] = branchSegmentDataDict
+        return trunkDict
 
     def _thicken(self):
         self.isThick = True
         for data in self.trunkDict.values():
             data["symbol"] = self._getTrunkChar()
 
+    def _createNewBranchOriginAtCoords(self, coords):
+        branchOriginDataDict = self.trunkDict[coords]
+
+        # Transform branchOriginDataDict accordingly
+        branchOriginDataDict["type"]["trunk"]["isBranchRoot"] = True
+
+        # Instantiate branch object
+        branchObject = Branch(branchOriginDataDict)
+
+        # Save branch object in trunk dict at coords
+        branchOriginDataDict["type"]["trunk"]["branchObject"] = branchObject
+
+        # Save branch object in branches origins dict
+        self.branchObjectsDict[coords] = branchObject
+
     def _beginBranching(self):
-        pass
+        branchNodeCoords = (-self.highestTrunk, 0)
+        self._createNewBranchOriginAtCoords(branchNodeCoords)
 
     def _lengthen(self):
         self.highestTrunk += 1
