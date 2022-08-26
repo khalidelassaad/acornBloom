3a3313281814c6add7c8f06b462eb046fa7d24ba
Khalid Elassaad <khalidelassaad@gmail.com>
2022-08-26 14:37:13 -0700

    code: trunk grows from bottom (debug chars)

--- tree/trunk.py
+++ tree/trunk.py
@@ -11,10 +11,11 @@ class Trunk:
         self.trunkDict = {
             (0, 0): originDataDict
         }
-        self.branchObjectsDict = dict()
+        self.branchObjectsList = []
         self.thickeningThreshold = random.choice(range(3, 6))
-        self.branchingThreshold = random.choice(range(2, 4))
+        self.branchingThreshold = random.choice(range(8, 9))
         self.lengthenProbability = 1
+        self.insertTrunkSegmentAtBottomProbability = 1
         self.isThick = False
         self.highestTrunk = 0
 
@@ -30,11 +31,15 @@ class Trunk:
 
     def getTrunkDict(self):
         trunkDict = self.trunkDict.copy()
-        for branchCoords, branchObject in self.branchObjectsDict.items():
-            for branchSegmentCoords, branchSegmentDataDict in branchObject.getBranchDict().items():
-                trunkDictCoords = tuple(sum(x) for x in zip(
-                    branchCoords, branchSegmentCoords))
-                trunkDict[trunkDictCoords] = branchSegmentDataDict
+        for trunkCoords, trunkSegmentData in self.trunkDict.items():
+            if trunkSegmentData["type"].get("trunk"):
+                if trunkSegmentData["type"]["trunk"]["isBranchRoot"]:
+                    branchCoords = trunkCoords
+                    branchObject = trunkSegmentData["type"]["trunk"]["branchObject"]
+                    for branchSegmentCoords, branchSegmentDataDict in branchObject.getBranchDict().items():
+                        trunkDictCoords = tuple(sum(x) for x in zip(
+                            branchCoords, branchSegmentCoords))
+                        trunkDict[trunkDictCoords] = branchSegmentDataDict
         return trunkDict
 
     def _thicken(self):
@@ -55,7 +60,7 @@ class Trunk:
         branchOriginDataDict["type"]["trunk"]["branchObject"] = branchObject
 
         # Save branch object in branches origins dict
-        self.branchObjectsDict[coords] = branchObject
+        self.branchObjectsList.append(branchObject)
 
     def _beginBranching(self):
         branchNodeCoords = (-self.highestTrunk, 0)
@@ -63,14 +68,27 @@ class Trunk:
 
     def _lengthen(self):
         self.highestTrunk += 1
-        newTrunkCoords = (-self.highestTrunk, 0)
+        DEBUGinsertAtBottom = random.random(
+        ) < self.insertTrunkSegmentAtBottomProbability and self.highestTrunk > 3
+        if DEBUGinsertAtBottom:
+            newTrunkCoords = (-1, 0)
+            for i in range(self.highestTrunk, 1, -1):
+                # Shift trunkDict "up 1", all except (0,0)
+                # Get trunkDict items, sort based on height coord (highest first),
+                # walk through and waterfall reassign until (-1, 0)
+                currentCoords = (-i, 0)
+                belowCoords = (-(i-1), 0)
+                self.trunkDict[currentCoords] = self.trunkDict[belowCoords]
+        else:
+            newTrunkCoords = (-self.highestTrunk, 0)
+
         self.trunkDict[newTrunkCoords] = {
             "type": {
                 "trunk": {
                     "isBranchRoot": False
                 }
             },
-            "symbol": self._getTrunkChar()
+            "symbol": [self._getTrunkChar(), "%"][DEBUGinsertAtBottom]
         }
 
     def _getTrunkChar(self):
