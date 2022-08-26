80d0c6f8436e0dbae98c8621cd5903a96f5efc94
Khalid Elassaad <khalidelassaad@gmail.com>
2022-08-26 15:29:37 -0700

    code: direction bullshit mapped

--- tree/branches.py
+++ tree/branches.py
@@ -1,6 +1,13 @@
 import random
 
 
+class BranchNode:
+    def __init__(self, direction, children):
+        self.direction = direction
+        self.children = children
+        self.hasBranched = False
+
+
 class Branches:
     # DIRECTIONS:
     #     2 3 4
@@ -9,7 +16,7 @@ class Branches:
     #      / \
     #     0   6
 
-    mapDirectionStepToCoordDelta = {
+    mapGrowthDirectionToCoordDelta = {
         0: (1, -1),
         1: (0, -1),
         2: (-1, -1),
@@ -29,15 +36,35 @@ class Branches:
         6: "\\"
     }
 
+    mapDirectionToBranchDirections = {
+        0: [1],
+        1: [0, 2],
+        2: [1, 3],
+        3: [2, 4],
+        4: [3, 5],
+        5: [4, 6],
+        6: [5]
+    }
+
+    mapBranchDirectionToCoordDelta = {
+        (0, 1): (0, -1),
+        (1, 0): (1, -1),
+        (1, 2): (-1, -1),
+        (2, 1): (0, -1),
+        (2, 3): (-1, 0),
+        (3, 2): (-1, -1),
+        (3, 4): (-1, 1),
+        (4, 3): (-1, 0),
+        (4, 5): (0, 1),
+        (5, 4): (-1, 1),
+        (5, 6): (1, 1),
+        (6, 5): (0, 1)
+    }
+
     def __init__(self, branchOriginDataDict):
-        firstBranch = random.choice([((0, 1), "/"), ((0, -1), "\\")])
-
-        self.branchDict = {
-            (0, 0): branchOriginDataDict,
-            firstBranch[0]: {"symbol": firstBranch[1]}
-        }
-
-        # TODO: add a branch!
+        leftBranchNode = BranchNode(2, None)
+        rightBranchNode = BranchNode(4, None)
+        self.rootBranchNode = BranchNode(3, [leftBranchNode, rightBranchNode])
 
     def handleAging(self):
         pass
